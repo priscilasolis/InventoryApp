b22bdd64d9edf3fb38666fbc38ead8175b03759d
Priscila <prisci_sg@hotmail.com>
2017-04-24 14:13:09 -0600

    Intento Dashboard

---
+++ InventoryApp/Controllers/DashboardController.cs
@@ -0,0 +1,27 @@
+﻿using InventoryApp.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InventoryApp.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly InventoryRepository _repository = new InventoryRepository();
+
+        public ActionResult MostSold()
+        {
+            return View(_repository.GetMostSoldWeek());
+        }
+
+        public ActionResult LowThreshold()
+        {
+
+            return View(_repository.GetItemsByThreshold());
+        }
+        
+    }
+}
+
